fd619dde93576da8f1abc1663131d0cf3f6ca8fd
vaibhav3534 <vaibhavsuryawanshi015@gmail.com>
2022-09-06 19:51:12 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import React from "react"
+import {Route, Routes} from "react-router-dom"
+
+import './App.css';
+import { Footer } from './components/Footer';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <h1>Main Section </h1>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      Footer
+      {/* <Routes>
+        <Route path="/" element={<Footer/>} />
+      </Routes> */}
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
