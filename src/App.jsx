1ac50c2662e6e41b2eb9f47112620a10d917809c
ayush-kr05 <ayushkumar.barbighaiya@gmail.com>
2022-09-06 18:35:13 +0530

    Navbar UI Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div style={{ height: "1500px" }}>Gapping only</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar/utils/Button.jsx
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  font-family: "lato regular", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  line-height: 1.2;
+  margin-bottom: 10px;
+  color: #000;
+  font-weight: 400;
+  letter-spacing: 0;
+  text-transform: none;
+  box-sizing: border-box;
+  background-color: #222;
+  color: #fff;
+  border-width: 2px;
+  border-style: solid;
+  border-color: #222;
+  border-radius: 1px;
+  display: inline-block;
+  text-decoration: none;
+  transition: background-color 0.2s;
+  padding: 9.6px 80px;
+  cursor: pointer;
+  text-align: center;
+  text-transform: uppercase;
+  box-sizing: border-box;
+
+  &: hover {
+    font-family: "lato regular", Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    line-height: 1.2;
+    margin-bottom: 10px;
+    font-weight: 400;
+    letter-spacing: 0;
+    text-transform: none;
+    box-sizing: border-box;
+    background-color: rgb(110, 103, 103);
+    color: #fff;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #222;
+    border-radius: 1px;
+    display: inline-block;
+    text-decoration: none;
+    transition: background-color 0.2s;
+    padding: 9.6px 80px;
+    cursor: pointer;
+    text-align: center;
+    text-transform: uppercase;
+    box-sizing: border-box;
+  }
+`;
+
+export const ButtonOpp = styled.button`
+  font-family: "lato regular", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  line-height: 1.2;
+  margin-bottom: 10px;
+  font-weight: 400;
+  letter-spacing: 0;
+  text-transform: none;
+  box-sizing: border-box;
+  background-color: white;
+  color: black;
+  border-width: 2px;
+  border-style: solid;
+  border-color: #222;
+  border-radius: 1px;
+  display: inline-block;
+  text-decoration: none;
+  transition: background-color 0.2s;
+  padding: 9.6px 66px;
+  cursor: pointer;
+  text-align: center;
+  text-transform: uppercase;
+  box-sizing: border-box;
+
+  &: hover {
+    font-family: "lato regular", Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    line-height: 1.2;
+    margin-bottom: 10px;
+    font-weight: 400;
+    letter-spacing: 0;
+    text-transform: none;
+    box-sizing: border-box;
+    background-color: #cccccc;
+    color: black;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #222;
+    border-radius: 1px;
+    display: inline-block;
+    text-decoration: none;
+    transition: background-color 0.2s;
+    padding: 9.6px 66px;
+    cursor: pointer;
+    text-align: center;
+    text-transform: uppercase;
+    box-sizing: border-box;
+  }
+`;
