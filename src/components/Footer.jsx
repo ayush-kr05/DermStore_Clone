9a01fdca003f01913a91ddf6a1757cd05da30742
Vaibhav3534 <107241783+Vaibhav3534@users.noreply.github.com>
2022-09-07 13:29:00 +0530

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,14 +11,12 @@ export const Footer = () => {
         <>
             <hr />
             <div id="main">
-           
-                <div id="div6">
-                    <p id="para1">Sign up to our newsletters and receive the latest exclusive discounts and deals</p>
+                <div id="upper_footer_left">
+                    <p id="upper_footer_heading">Sign up to our newsletters and receive the latest exclusive discounts and deals</p>
                     <button type="button" className="btn-dar" onClick={()=>{
-                        // navigate("/")
                     }}>SIGN ME UP</button>
                 </div>
-                <div id="div2">
+                <div id="upper_footer_right">
                     <p className="connect_us">Connect with us</p>
                     <div className="icon" >
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
@@ -34,24 +32,24 @@ export const Footer = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
                                 <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
                             </svg> <span>
-                                {/* <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pinterest" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pinterest" viewBox="0 0 16 16">
                                     <path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0z" />
-                                </svg> */}
+                                </svg>
                             </span>
                         </span>
-                        {/* <hr width="100%" /> */}
                     </div>
 
                 </div>
+                <br />
 
             </div>
 
             <section>
                 <hr />
-                <div id="div3">
+                <div id="list_left">
                         <hr />
 
-                    <p id="para3">Customer Service</p>
+                    <p id="list_heading">Customer Service</p>
                     <ul>
                         <li><a href="#">Help Center / FAQs</a></li>
                         <li><a href="#">Returns</a></li>
@@ -61,40 +59,38 @@ export const Footer = () => {
                     </ul>
 
                 </div>
-                <div id="div4">
+                <div id="list_right">
                     <div className="child">
                         <hr />
-                        <p id="para3">Customer Service</p>
+                        <p id="list_heading">My Account</p>
                         <ul>
-                            <li><a href="#">Help Center / FAQs</a></li>
-                            <li><a href="#">Returns</a></li>
-                            <li><a href="#">Shipping Information</a></li>
-                            <li><a href="#">Order Status</a></li>
-                            <li><a href="#">Cookie Settings</a></li>
+                            <li><a href="#">Manage My Auto-Replenishments</a></li>
+                            <li><a href="#">My Rewards</a></li>
+                            <li><a href="#">My Favorites</a></li>
+                            <li><a href="#">Refer a Friend</a></li>
+                            <li><a href="#">Order History</a></li>
                         </ul>
 
                     </div>
                     <div className="child">
                         <hr />
-                        <p id="para3">Customer Service</p>
+                        <p id="list_heading">Company</p>
                         <ul>
-                            <li><a href="#">Help Center / FAQs</a></li>
-                            <li><a href="#">Returns</a></li>
-                            <li><a href="#">Shipping Information</a></li>
-                            <li><a href="#">Order Status</a></li>
-                            <li><a href="#">Cookie Settings</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Press</a></li>
+                            <li><a href="#">What is Klarna?</a></li>
                         </ul>
 
                     </div>
                     <div className="child">
                         <hr />
-                        <p id="para3">Customer Service</p>
+                        <p id="list_heading">Legal</p>
                         <ul>
-                            <li><a href="#">Help Center / FAQs</a></li>
-                            <li><a href="#">Returns</a></li>
-                            <li><a href="#">Shipping Information</a></li>
-                            <li><a href="#">Order Status</a></li>
-                            <li><a href="#">Cookie Settings</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Terms & Conditions</a></li>
+                            <li><a href="#">Modern Slavery Statement</a></li>
+                            <li><a href="#">Product Recall</a></li>
+                            <li><a href="#">Accessibility</a></li>
                         </ul>
 
                     </div>
@@ -105,13 +101,13 @@ export const Footer = () => {
 
 <hr />
 
-                <div id="main3">
-                    <div id="div8">
+                <div id="bottom_section">
+                    <div id="THG_div">
 
                         <p id="thg">T H G</p>
                         <p>2022 © The Hut.com Ltd.</p>
                     </div>
-                    <div id="div7">
+                    <div id="pay_secure_div">
                         <div>
                         <h2>Pay securely with</h2>
                         </div>
