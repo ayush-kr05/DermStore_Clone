99d318c8bdc754d9fe1216ec2e8c130a0a0ff130
Vaibhav3534 <107241783+Vaibhav3534@users.noreply.github.com>
2022-09-08 20:50:59 +0530

    Product Page

---
+++ src/components/categeory.jsx
@@ -0,0 +1,42 @@
+
+export const Categeory = {
+    Category : [
+        ["Moisturizers",2],["Face Makeup",4],["Eye Care",3],["Blush",3],["Illamasqua",1],["jane iredale",2],["Jouer Cosmetics",2],["Kevyn Aucoin",1],["Kjaer Weis",5],["Perricone MD",1],["RMS Beauty",2],["Smashbox",2],["Stila",1],["Supergoop!",1],["Tarte",0],["Vapour Beauty",3],
+    ],
+    Makeup_Category : [
+         "Lips","Makeup", "Eyes","Palettes","Complexion"
+    ],
+    Makeup_Product_Type : [
+        "Blusher (5)",
+    ],
+    Complexion_Format : [
+        "Cream (4)","Liquid (3)","Stick (7)",
+    ],
+    Complexion_Finish : [
+        "Dewy (1)","Glossy (2)", "Matte (9)", "Satin (6)", 
+    ],
+    Complexion_Coverage : [
+        "Buildable (7)", "Full (5)", "Medium (1)", "Sheer (3)",
+    ],
+    Cosmetics_TA : [
+        "Brushes (2)",
+    ],
+    Cosmetics_BT : [
+        "Lip (2)",
+    ],
+    Lips_Products : [
+        "Lipstick (35)", "Liquid Lipstick (4)", "Lip Gloss (3)", "Lip Stain & Tint (3)", "Lip Plumper (1)", "Tinted Lip Balm (1)", 
+    ],
+    Preferences : [
+        "Certified Cruelty-Free (22)", "Certified Organic (6)", "Clean (13)", "Fragrance-Free (4)", "Gluten-Free (12)", "Hypoallergenic (4)", "Natural (5)", "Non-Comedogenic", "Paraben-Free (10)", "Phthalate-Free (11)", "Sulfate-Free (11)", "Vegan (10)",
+    ],
+    Price : [
+        "$10 - $25 (11)","$25 - $50 (25)","78",
+    ],
+    Savings : [
+        "Up to 25% (2)", "25% - 50% (1)", 
+    ],
+    Average_Reviews : [
+        "4+ (27)",
+    ],
+}
