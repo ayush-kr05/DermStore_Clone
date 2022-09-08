eb90f4ae28fc9e6aebdaf079f87ba63f3766c37e
krishnachaturvedi1901 <krishna.chaturvedi1901@gmail.com>
2022-09-08 21:26:14 +0530

    work thursday

--- src/Pages/Product_Overview/ProductOverview.jsx
+++ src/Pages/Product_Overview/ProductOverview.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Navbar } from "../../Components/Navbar/Navbar";
+import { Footer } from "../../Components/Footer/Footer";
 
 export const ProductOverview = () => {
   const { id } = useParams();
+  const [data,setData]=React.useState({})
+
+  const getSearchData = () => {
+    fetch(`http://localhost:8080/products?q=${id}`)
+      .then((res) => res.json())
+      .then((res) => setData(res[0]))
+      .catch((error) => console.log(error));
+  };
+  React.useState(()=>{
+    getSearchData()
+  },[])
+
+  console.log(data)
   return (
-    <div>
-      ProductOverview
-      <p>{id}</p>
-    </div>
+    <>
+    <Navbar/>
+     <div>
+       ProductOverview
+       <p>{id}</p>
+     </div>
+     <Footer/>
+    </>
   );
 };
